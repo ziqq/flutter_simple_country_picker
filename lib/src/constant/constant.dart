0ec80ecd8adadb4912652ea35dc9f6a4eb5fdcfe
AntØn UstinØff <a.a.ustinoff@gmail.com>
2024-10-07 10:51:37 +0400

    feat: add default constants

---
+++ lib/src/constant/constant.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+/// Default indent
+const double kDefaultIndent = 10;
+
+/// Default padding
+const double kDefaultPadding = 16;
+
+/// Default margin
+const double kDefaultBorderRadius = 10;
+
+/// Default text style
+const TextStyle kDefaultTextStyle = TextStyle(fontSize: 16);
+
+/// Default phone mask
+const String kDefaultPhoneMask = '000 000 0000';
