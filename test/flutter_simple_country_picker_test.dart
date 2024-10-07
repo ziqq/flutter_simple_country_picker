26379656cbb915b2813ec3781c16eede64b5568c
AntØn UstinØff <a.a.ustinoff@gmail.com>
2024-10-07 11:08:24 +0400

    feat: add first tests

--- test/flutter_simple_country_picker_test.dart
+++ test/flutter_simple_country_picker_test.dart
@@ -1,9 +1,11 @@
-import 'package:flutter_simple_country_picker/flutter_simple_country_picker.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'unit_test/flutter_simple_country_picker_test.dart' as unit_test;
+import 'widget_test/flutter_simple_country_picker_test.dart' as widget_test;
+
 void main() {
-  test('First_test', () {
-    // ignore: unused_local_variable
-    final picker = FlutterSimpleCountryPicker();
+  group('flutter_simple_country_picker -', () {
+    group('unit_test -', unit_test.main);
+    group('widget_test -', widget_test.main);
   });
 }
---
+++ test/widget_test/flutter_simple_country_picker_test.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Example_first_widget_test -', (tester) async {});
+}
