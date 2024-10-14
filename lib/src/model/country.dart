9ee9895f82eab3d5ef036c31526c0382a08f286d
AntØn UstinØff <a.a.ustinoff@gmail.com>
2024-10-14 18:01:30 +0400

    feat: add mock

--- lib/src/model/country.dart
+++ lib/src/model/country.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_simple_country_picker/flutter_simple_country_picker.dart';
+import 'package:flutter_simple_country_picker/src/constant/country_code/country_codes.dart';
 import 'package:flutter_simple_country_picker/src/controller/countries_parser.dart';
 import 'package:flutter_simple_country_picker/src/util/countries_util.dart';
 import 'package:meta/meta.dart';
@@ -40,6 +41,9 @@ class Country {
     this.fullExampleWithPlusSign,
     this.mask,
   });
+
+  /// Create a mock countries.
+  factory Country.mock() => Country.fromJson(countries[0]);
 
   /// Create a country from a JSON object
   Country.fromJson(Map<String, Object?> json)
