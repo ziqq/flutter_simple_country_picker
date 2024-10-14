6520f19fe67f479cced113e82ba9fea367b834b1
AntØn UstinØff <a.a.ustinoff@gmail.com>
2024-10-14 17:01:28 +0400

    chore: provide out countries state

--- lib/flutter_simple_country_picker.dart
+++ lib/flutter_simple_country_picker.dart
@@ -1,6 +1,7 @@
 library flutter_simple_country_picker;
 
 export 'src/constant/typedef.dart';
+export 'src/controller/countries_state.dart';
 export 'src/localization/localization.dart';
 export 'src/model/country.dart';
 export 'src/theme/country_picker_theme.dart';
